217e984203625334967b9600f23c3acb49e9c577
Miha Fuderer <m.fuderer@umcutrecht.nl>
2025-03-05 13:12:46 +0100

    Fixing Manifest/Project for ScanAnalysis (plus adding some code for Simulation1D)

---
+++ Simulation/setup.jl
@@ -0,0 +1,19 @@
+using ComputationalResources
+# using CUDA
+using PyPlot 
+# using PyCall
+
+cpu  = ComputationalResources.CPU1()
+using BlochSimulators
+# using Colors
+# using Statistics
+using MRSTATToolbox
+
+# using JLD2 
+# using MAT
+# using Serialization
+# using StaticArrays, StructArrays
+# using Random
+# using FFTW, FourierTools
+# using Interpolations
+# using Printf
